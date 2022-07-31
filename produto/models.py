f80ed84193911bec85c99bee5e685a67b5722f6b
Rodrigo Barros <brodrigo87@gmail.com>
2022-07-31 11:39:37 -0300

    Produto models

--- produto/models.py
+++ produto/models.py
@@ -2,19 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-"""""
-        Produto:
-            nome - Char
-            descricao_curta - Text
-            descricao_longa - Text
-            imagem - Image
-            slug - Slug
-            preco_marketing - Float
-            preco_marketing_promocional - Float
-            tipo - Choices
-                ('V', 'Variável'),
-                ('S', 'Simples'),
-"""
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variação')
+            ('S', 'Simples'),
+        )
+    )
 
 
 """""
