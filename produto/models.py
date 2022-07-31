a083010a168433205bfe505483fdc2dcd0ac70d4
Rodrigo Barros <brodrigo87@gmail.com>
2022-07-31 11:30:56 -0300

    models txt

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+"""""
+        Produto:
+            nome - Char
+            descricao_curta - Text
+            descricao_longa - Text
+            imagem - Image
+            slug - Slug
+            preco_marketing - Float
+            preco_marketing_promocional - Float
+            tipo - Choices
+                ('V', 'Variável'),
+                ('S', 'Simples'),
+"""
+
+
+"""""
+        Variacao:
+            nome - char
+            produto - FK Produto
+            preco - Float
+            preco_promocional - Float
+            estoque - Int
+
+"""
