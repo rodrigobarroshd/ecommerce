593bdbd5ee39a62b1ecef2215e65fe5d43a63fbe
Rodrigo Barros <brodrigo87@gmail.com>
2022-08-12 11:23:15 -0300

    perfil url view

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
+from . import views
+
 
 
 urlpatterns = [
-    
+    path('', views.Criar.as_view(), name='criar'),
+    path('atualizar/', views.Atualizar.as_view(), name='atualizar'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout', views.Logout.as_view(), name='logout'),
+
+
+
 ]
+
+
+# perfil:
+#     '' - criar
+#     update
+#     login
+#     logout
