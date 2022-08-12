30b3b7d004aff66eff1b27d7b2b72ea43600944d
Rodrigo Barros <brodrigo87@gmail.com>
2022-08-12 11:41:49 -0300

    test views url

--- produto/views.py
+++ produto/views.py
@@ -1,29 +1,35 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
 
 
-class ListaProdutos(ListView):
-    pass
+class ListaProdutos(View):  # alterar pra ListView
+    def get(self, *args, **kwargs):
+        return HttpResponse('Listar')
 
 
 class DetalheProduto(View):
-    pass
-
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe')
 
 class AdicionarAoCarrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        return HttpResponse('Adicionar Ao Carrinho')
 
 
 class RemoverDoCarrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        return HttpResponse('Remover Do Carrinho')
 
 
 class Carrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
 
 
 class Finalizar(View):
-    pass
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
