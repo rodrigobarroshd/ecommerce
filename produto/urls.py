7528b6c3083d641437319887cacae4914db7ced7
Rodrigo Barros <brodrigo87@gmail.com>
2022-08-12 11:15:50 -0300

    produto url view

--- produto/urls.py
+++ produto/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+from . import views
+
+
+app_name = 'produto' # produto lista
 
 
 urlpatterns = [
-    
+    path('', views.ListaProdutos.as_view(), name='Lista'),
+    path('<slug>', views.DetalheProduto.as_view(), name='detalhe'),
+    path('adicionaraocarrinho', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
+    path('removerdocarrinho', views.RemoverDoCarrinho.as_view(), name='removerdocarrinho'),
+    path('carrinho', views.Carrinho.as_view(), name='carrinho'),
+    path('finalizar', views.Finalizar.as_view(), name='finalizar'),
+
+
 ]
