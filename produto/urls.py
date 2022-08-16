2ae6c01dd5abf85e299ab468b4222af561892733
Rodrigo Barros <brodrigo87@gmail.com>
2022-08-16 16:14:04 -0300

    carrinho

--- produto/urls.py
+++ produto/urls.py
@@ -6,7 +6,7 @@ app_name = 'produto' # produto lista
 
 
 urlpatterns = [
-    path('', views.ListaProdutos.as_view(), name='Lista'),
+    path('', views.ListaProdutos.as_view(), name='lista'),
     path('<slug>', views.DetalheProduto.as_view(), name='detalhe'),
     path('adicionaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
     path('removerdocarrinho/', views.RemoverDoCarrinho.as_view(), name='removerdocarrinho'),
